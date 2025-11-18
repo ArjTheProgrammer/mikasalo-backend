fb7a0a1bdad091451ae5fbb9436bc8a647c4e68b
Jose Neil Silagan <neilsilagan@gmail.com>
2025-11-18 21:27:23 +0800

    Refactor Menu Schema: Add imageUrl and isAvailable
    - Also change the Name of the Menu interface
    - Wrong naming to the toNewMenu function

--- src/utils/validations/menu.schema.ts
+++ src/utils/validations/menu.schema.ts
@@ -6,23 +6,26 @@ export enum Category {
     Dessert = 'dessert'
 }
 
-export interface MenuItem {
+export interface Menu {
     id: number,
     name: string,
     description: string,
     price: number,
-    category: Category
+    category: Category,
+    imageUrl?: string,
+    isAvailable: boolean
 }
 
 export const NewMenuSchema = z.object({
     name: z.string(),
     description: z.string(),
     price: z.number(),
-    category: z.enum(Category)
+    category: z.enum(Category),
+    imageUrl: z.string().optional()
 })
 
 export type newMenu = z.infer<typeof NewMenuSchema>;
 
-export const toNewUser = (object: unknown): newMenu => {
+export const toNewMenu = (object: unknown): newMenu => {
     return NewMenuSchema.parse(object)
 }
