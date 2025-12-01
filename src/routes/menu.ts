4d1a7c338fbac6dcf1048d02c7617f9bc651982a
Jose Neil Silagan <neilsilagan@gmail.com>
2025-12-01 11:47:34 +0800

    Refactor: Change the wrong name for the variables of menu routes

--- src/routes/menu.ts
+++ src/routes/menu.ts
@@ -7,17 +7,17 @@ const router = express.Router();
 
 router.get('/', async (_req: Request, res: Response) => {
   try {
-    const users = await menuService.getAllMenu();
-    res.json(users);
+    const menu = await menuService.getAllMenu();
+    res.json(menu);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch users' });
+    res.status(500).json({ error: `Failed to fetch menu ${error}` });
   }
 });
 
 router.post('/', newMenuParser, async (req: Request<unknown, unknown, newMenu>, res: Response) => {
   try {
-    const savedUser = await menuService.createMenu(req.body);
-    res.status(201).json(savedUser);
+    const savedMenu = await menuService.createMenu(req.body);
+    res.status(201).json(savedMenu);
   } catch (error) {
     if (error instanceof Error) {
       res.status(400).json({ error: error.message });
